45d3156f0e04f56e9aba1c0150271a5bfa1da85b
dasMichal <michalxd@live.de>
2019-11-11 23:51:45 +0100

    Gewinnlogik

--- src/connect_four.java
+++ src/connect_four.java
@@ -1,57 +1,77 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class connect_four {
 
 
     public static void main(String[] args) {
 
-        //char x = X;
         //char o = O;
         //char anzahl= 0;
         int rowy = 0;
         int rowx = 0;
+        int auswahl;
+
         Scanner input = new Scanner(System.in);
         // create an array with the size of the needed connect four field
         char[][] feld = new char[6][7];
         char[] anzahl = new char[7];
-
-        System.out.println("CONNECT FOUR\n");
-
         for (int i = 0; i < 6; i++) {       //erzeugt array zum überprüfen wie viele Chips in einem Feld sind
-            anzahl[i] = 0;
+            anzahl[i] = 5;
         }
 
 
-
-        do {
-            System.out.print("\nWhich ROW ?"); //X
-            rowx = input.nextInt();
-            //System.out.print("\nWhich Hight ?"); //Y
-            //rowy = input.nextInt();
-            if ((rowy <= 6) && (rowx <= 7)) {
-                rowy = rowy - 1;   //Y
-                rowx = rowx - 1;  //X
+        System.out.println("CONNECT FOUR\n");
 
 
-                //char y = anzahl[rowy];
-                char y = ((char) rowy);
-                char x = ((char) rowx);
-                feld[anzahl[rowx]][x] = 'X';
-                //i = (char) (i + 1);
-                anzahl[rowx]++;
+            /* Hauptmenue */
+            System.out.print("\n");
+            System.out.print("-----------------------------------------------\n");
+            System.out.print("1.  Start New Game\n");
+            System.out.print("9.  Exit\n");
+            System.out.print("-----------------------------------------------\n\n");
+            System.out.print("Enter Your Choice :  ");
+            auswahl = input.nextInt();
+
+            if (auswahl == 9) System.exit(0);
+            else
+                {
+
+                    startGame(feld,anzahl,rowx,rowy,input,auswahl);
+
+                }
 
 
-            }
+
+
+            /*
             showField(feld);
-        }while(rowx !=9);
+            PlayerX(feld, anzahl, rowx, rowy, input);
+            showField(feld);
+            check(feld, anzahl, rowx,auswahl);
+            PlayerO(feld, anzahl, rowx, rowy, input);
+            showField(feld);
+            check(feld, anzahl, rowx,auswahl);
+            */
 
 
-        showField(feld);
 
 
     }
 
-    public static void showField (char[][] feld) {
+    private static void startGame(char[][] feld, char[] anzahl, int rowx, int rowy, Scanner input, int auswahl){
+
+        do {
+            showField(feld);
+            PlayerX(feld, anzahl, rowx, rowy, input);
+            showField(feld);
+            check(feld, anzahl, rowx, auswahl);
+            PlayerO(feld, anzahl, rowx, rowy, input);
+            showField(feld);
+            check(feld, anzahl, rowx, auswahl);
+        }while(auswahl !=9);
+    }
+
+    public static void showField(char[][] feld) {
         // Function that prints the field represented in an array
 
         System.out.println(" 1 2 3 4 5 6 7");
@@ -69,6 +89,92 @@ public class connect_four {
 
 
     }
-}
 
-public static void PlayerX()
+
+    public static void PlayerX(char[][] feld, char[] anzahl, int rowx, int rowy, Scanner input) {
+        System.out.print("PLAYER X");
+        System.out.print("\nWhich ROW ? "); //X
+        rowx = input.nextInt();
+        //System.out.print("\nWhich Hight ?"); //Y
+        //rowy = input.nextInt();
+        if (rowx <= 7) {
+            //rowy = rowy - 1;   //Y
+            rowx = rowx - 1;  //X
+
+
+            //char y = anzahl[rowy];
+            //char y = ((char) rowy);
+            char x = ((char) rowx);
+            feld[anzahl[rowx]][x] = 'X';
+            //i = (char) (i + 1);
+            anzahl[rowx]--;
+
+        }
+    }
+
+    public static void PlayerO(char[][] feld, char[] anzahl, int rowx, int rowy, Scanner input) {
+        System.out.print("PLAYER O");
+        System.out.print("\nWhich ROW ? "); //X
+        rowx = input.nextInt();
+        //System.out.print("\nWhich Hight ?"); //Y
+        //rowy = input.nextInt();
+        if (rowx <= 7) {
+            //rowy = rowy - 1;   //Y
+            rowx = rowx - 1;  //X
+
+
+            //char y = anzahl[rowy];
+            //char y = ((char) rowy);
+            char x = ((char) rowx);
+            feld[anzahl[rowx]][x] = 'O';
+            //i = (char) (i + 1);
+            anzahl[rowx]--;
+
+        }
+    }
+
+    public static void check(char[][] feld, char[] anzahl, int rowx, int auswahl) {
+
+
+        int count = 0;
+        int i;
+
+        for (i = 0; i < 6; i++) {           //Check for X
+            for (int j = 0; j < 7; j++) {
+                if (feld[i][j] == 'X')
+
+                    count++;
+                //System.out.println(count);
+
+                if (count == 4) {
+
+                    System.out.println("WINNER");
+                    auswahl =9;
+                    //System.exit(0);
+
+
+                }
+            }
+        }
+
+        count = 0;
+
+        for (i = 0; i < 6; i++) {       //Check for O
+            for (int j = 0; j < 7; j++) {
+                if (feld[i][j] == 'O')
+
+                    count++;
+                //System.out.println(count);
+
+                if (count == 4) {
+
+
+                    System.out.println("WINNER");
+                    auswahl =9;
+                    //System.exit(0);
+
+                }
+            }
+        }
+    }
+}
